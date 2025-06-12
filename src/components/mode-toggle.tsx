83c59ffd5084bce69b1c9b61d02ac2dfaf64e700
icantcodefyi <venti.sillly@gmail.com>
2025-06-12 14:38:03 +0530

    add TIFF to supported image file types and make mode toggle simple

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,43 +1,32 @@
-import React from "react"
-import { Moon, Sun } from "lucide-react"
+import React from "react";
+import { Moon, Sun } from "lucide-react";
 
-import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { useTheme } from "@/components/theme-provider"
+import { Button } from "@/components/ui/button";
+import { useTheme } from "@/components/theme-provider";
 
 export function ModeToggle() {
-  const { theme, setTheme } = useTheme()
-  
-  const isDark = theme === "dark" || (theme === "system" && window.matchMedia("(prefers-color-scheme: dark)").matches)
+  const { theme, setTheme } = useTheme();
+
+  const isDark =
+    theme === "dark" ||
+    (theme === "system" &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches);
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          {isDark ? (
-            <Moon className="h-[1.2rem] w-[1.2rem] transition-all duration-200" />
-          ) : (
-            <Sun className="h-[1.2rem] w-[1.2rem] transition-all duration-200" />
-          )}
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  )
-} 
+    <Button
+      variant="outline"
+      className={`border-none group  ${
+        isDark ? "hover:bg-neutral-800" : "hover:bg-yellow-100"
+      }`}
+      size="icon"
+      onClick={() => setTheme(isDark ? "light" : "dark")}
+    >
+      {isDark ? (
+        <Moon className="h-[1.2rem] w-[1.2rem] transition-all group-hover:text-neutral-500 duration-200" />
+      ) : (
+        <Sun className="h-[1.2rem] w-[1.2rem] transition-all duration-200 group-hover:text-yellow-500" />
+      )}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
